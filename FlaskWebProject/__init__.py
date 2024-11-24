cc7367473dd84f2aa3ec9a2bb4e2a489c80c62db
Vu Quoc Trung <linkinf88k@gmail.com>
2024-11-24 10:18:30 +0700

    update logger

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,6 +11,27 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+
+# Set the logging level
+app.logger.setLevel(logging.DEBUG)
+
+# Create a file handler to log messages to a file
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.INFO)  # Log messages of level INFO and above
+
+# Create a console handler to log messages to the console
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.ERROR)  # Log messages of level ERROR and above
+
+# Create a formatter and set it for both handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add the handlers to the app's logger
+app.logger.addHandler(file_handler)
+app.logger.addHandler(console_handler)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -63,14 +63,17 @@ def post(id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
+        app.logger.info(f'User {current_user.username} attempted to log in again.')
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.warning(f'Invalid login attempt for username: {form.username.data}')
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        app.logger.info(f'User {user.username} logged in successfully.')
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
@@ -101,6 +104,7 @@ def authorized():
 
 @app.route('/logout')
 def logout():
+    app.logger.info(f'User {current_user.username} logged out.')
     logout_user()
     if session.get("user"): # Used MS Login
         # Wipe out user and its token cache from session
